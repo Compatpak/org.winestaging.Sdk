f63d24e634e229c12389704bcb7256ff0f722319
Chris Hobday <hobday.chris@protonmail.com>
2026-07-23 15:52:47 -0400

    change x32 dir to x86

--- Scripts/WineToVKD3D-Proton.sh
+++ Scripts/WineToVKD3D-Proton.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cp /usr/share/vkd3d-proton/x64/*.dll $WINEPREFIX/drive_c/windows/system32/ # Copy 64bit VKD3D-Proton dlls, overwriting default Wine dlls
-cp /usr/share/vkd3d-proton/x32/*.dll $WINEPREFIX/drive_c/windows/syswow64/ # Copy 32bit VKD3D-Proton dlls, overwriting default Wine dlls
+cp /usr/share/vkd3d-proton/x86/*.dll $WINEPREFIX/drive_c/windows/syswow64/ # Copy 32bit VKD3D-Proton dlls, overwriting default Wine dlls
